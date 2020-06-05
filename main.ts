e6a61657dd6db6698cff7ab4a056ecec438ce0e9
Lionkk <Lion_yan@outlook.com>
2020-06-05 15:21:00 +0800

    Updates.

--- main.ts
+++ main.ts
@@ -47,6 +47,67 @@ namespace ErlangGod {
         //% block="Face order"
         Faceorder = 8
     }
+    /**
+    * Status List of Card
+    */
+    export enum CardState {
+        //% block="X"
+        X = 2,
+        //% block="Y"
+        Y = 3,
+        //% block="W"
+        W = 4,
+        //% block="H"
+        H = 5,
+        //% block="Confidence level "
+        Confidence = 6,
+        //% block="Card TotalNum"
+        CardTotalNum = 7,
+        //% block="Card order"
+        Cardorder = 8
+    }
+    /**
+* Status List of Color
+*/
+    export enum ColorState {
+        //% block="ID"
+        ID = 1,
+        //% block="X"
+        X = 2,
+        //% block="Y"
+        Y = 3,
+        //% block="W"
+        W = 4,
+        //% block="H"
+        H = 5,
+        //% block="Confidence level "
+        Confidence = 6,
+        //% block="Color TotalNum"
+        ColorTotalNum = 7,
+        //% block="Color order"
+        Colororder = 8
+    }
+    /**
+    * Status List of Things
+    */
+    export enum ThingsState {
+        //% block="ID"
+        ID = 1,
+        //% block="X"
+        X = 2,
+        //% block="Y"
+        Y = 3,
+        //% block="W"
+        W = 4,
+        //% block="H"
+        H = 5,
+        //% block="Confidence level "
+        Confidence = 6,
+        //% block="Things TotalNum"
+        ThingsTotalNum = 7,
+        //% block="Things order"
+        Thingsorder = 8
+    }
 
     export enum LineState {
         //% block="angel"
@@ -85,7 +146,7 @@ namespace ErlangGod {
     //% block="Get once data from ElangGod"
     //% group="Data"
     export function cameraData(): void {
-        DataBuff = pins.i2cReadBuffer(0x14, 9)
+        DataBuff = pins.i2cReadBuffer(CameraAdd, 9)
     }
     //% block="Recognize the ball"
     //% group="Ball"
@@ -187,22 +248,22 @@ namespace ErlangGod {
     //% state.fieldEditor="gridpicker"
     //% state.fieldOptions.columns=3
     //% group="Card"
-    export function CardData(state: FaceState): number {
+    export function CardData(state: CardState): number {
         if (DataBuff[0] == 2 || DataBuff[0] == 3 || DataBuff[0] == 4 || DataBuff[0] == 5) {
             switch (state) {
-                case FaceState.X:
+                case CardState.X:
                     return (DataBuff[2]);
-                case FaceState.Y:
+                case CardState.Y:
                     return (DataBuff[3]);
-                case FaceState.W:
+                case CardState.W:
                     return (DataBuff[4]);
-                case FaceState.H:
+                case CardState.H:
                     return (DataBuff[5]);
-                case FaceState.Confidence:
+                case CardState.Confidence:
                     return (DataBuff[6]);
-                case FaceState.FaceTotalNum:
+                case CardState.CardTotalNum:
                     return (DataBuff[7]);
-                case FaceState.Faceorder:
+                case CardState.Cardorder:
                     return (DataBuff[8]);
                 default:
                     return 0;
@@ -240,6 +301,98 @@ namespace ErlangGod {
                     return (DataBuff[3]);
                 case LineState.width:
                     return (DataBuff[4]);
+                default:
+                    return 0;
+            }
+        }
+        else
+            return null
+    }
+    //% block="learn color ID %colorID"
+    //% group="Color"
+    export function learnColor(colorID: number): void {
+        let ColorBuf = pins.createBuffer(9)
+        let timeout = 0
+        ColorBuf[0] = 9
+        ColorBuf[1] = colorID
+        pins.i2cWriteBuffer(CameraAdd, ColorBuf)
+        while (timeout > 10000) {
+            cameraData()
+            if (DataBuff[0] == 9 && DataBuff[1] == colorID) {
+                break
+            }
+            timeout++
+        }
+    }
+    //% block="From data Object Card state %state"
+    //% state.fieldEditor="gridpicker"
+    //% state.fieldOptions.columns=3
+    //% group="Card"
+    export function colorData(state: ColorState): number {
+        if (DataBuff[0] == 9) {
+            switch (state) {
+                case ColorState.ID:
+                    return DataBuff[1]
+                case ColorState.X:
+                    return DataBuff[2]
+                case ColorState.Y:
+                    return DataBuff[3]
+                case ColorState.W:
+                    return DataBuff[4]
+                case ColorState.H:
+                    return DataBuff[5]
+                case ColorState.Confidence:
+                    return DataBuff[6]
+                case ColorState.ColorTotalNum:
+                    return DataBuff[7]
+                case ColorState.Colororder:
+                    return DataBuff[8]
+                default:
+                    return 0;
+            }
+        }
+        else
+            return null
+    }
+    //% block="learn Things ID %thingsID"
+    //% group="Color"
+    export function learnThings(thingsID: number): void {
+        let thingsBuf = pins.createBuffer(9)
+        let timeout = 0
+        thingsBuf[0] = 9
+        thingsBuf[1] = thingsID
+        pins.i2cWriteBuffer(CameraAdd, thingsBuf)
+        while (timeout > 10000) {
+            cameraData()
+            if (DataBuff[0] == 9 && DataBuff[1] == thingsID) {
+                break
+            }
+            timeout++
+        }
+    }
+    //% block="From data Object things state %state"
+    //% state.fieldEditor="gridpicker"
+    //% state.fieldOptions.columns=3
+    //% group="Card"
+    export function thingsData(state: ThingsState): number {
+        if (DataBuff[0] == 9) {
+            switch (state) {
+                case ThingsState.ID:
+                    return DataBuff[1]
+                case ThingsState.X:
+                    return DataBuff[2]
+                case ThingsState.Y:
+                    return DataBuff[3]
+                case ThingsState.W:
+                    return DataBuff[4]
+                case ThingsState.H:
+                    return DataBuff[5]
+                case ThingsState.Confidence:
+                    return DataBuff[6]
+                case ThingsState.Thingsorder:
+                    return DataBuff[7]
+                case ThingsState.ThingsTotalNum:
+                    return DataBuff[8]
                 default:
                     return 0;
             }
